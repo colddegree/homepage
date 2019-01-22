185aa45762cf664c96dc8e79a1fb9d3a4faf6082
alexander <colddegree@yandex.ru>
2019-01-23 01:45:17 +1000

    [Index] welcome image added

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.svg$/,
+                test: /\.(svg|png)$/,
                 use: "file-loader"
             }
         ]
